3b80c44aad89f6be5c2a0ddf2b8a007fe8960334
ung <test@abc.com>
2023-07-08 18:30:24 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  /// 自定义报错页面
+  
   if (kReleaseMode) {
     ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails) {
       debugPrint(flutterErrorDetails.toString());
